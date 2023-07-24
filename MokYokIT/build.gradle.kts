d29a6986e1638351ab3280ac541821183912695d
jarosigor <55409506+jarosigor@users.noreply.github.com>
2023-07-24 16:37:29 +0200

    Removed unused dependencies.

--- MokYokIT/build.gradle.kts
+++ MokYokIT/build.gradle.kts
@@ -12,8 +12,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.2")
     implementation("net.minidev:json-smart:2.5.0")
 }
 
